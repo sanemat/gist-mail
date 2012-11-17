102c764f39043519bb3a7ba09f683470eb7952f0
Suguru Odai <ourqwers@gmail.com>
2012-11-17 17:35:49 +0900

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GistMail::Application.config.secret_token = '9a4ed0861dc61e239220865cf74e562baf26adbf8eecfbbe95abae8c00f54d6d6a3414f285801b16789a5418637a860dabc8d03e64d2d4862be2010d48a9ac9b'
