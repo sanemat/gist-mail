0fef9cfa9b9217b0fdd5d23f8e4ed28c3c8a1698
ryosuke shimomura <shimosuk.shimosuke@gmail.com>
2012-11-23 00:38:24 +0900

    schema.rbを更新
    rake db:migrate
    ticket

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121117084622) do
+ActiveRecord::Schema.define(:version => 20121121164551) do
+
+  create_table "gists", :force => true do |t|
+    t.string   "name"
+    t.string   "url"
+    t.integer  "count"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "users", :force => true do |t|
     t.string   "email",                  :default => "", :null => false
