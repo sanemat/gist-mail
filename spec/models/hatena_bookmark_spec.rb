b529fd9180c72b9da7c7f66cc2914340d67d36e5
sanemat <o.gata.ken@gmail.com>
2012-11-23 16:43:06 +0900

    Use specific time

--- spec/models/hatena_bookmark_spec.rb
+++ spec/models/hatena_bookmark_spec.rb
@@ -2,18 +2,21 @@
 require 'spec_helper'
 
 describe HatenaBookmark do
+  let(:now) { Time.zone.parse('2012-11-21 12:00+0900') }
+
   before do
-    time_now = Time.local(2012, 11, 21, 12, 0, 0)
-    Time.stub(:now).and_return time_now
+    Timecop.freeze(now)
     @hatena = HatenaBookmark.new
     hatebu_response = File.open File.join(fixture_path, 'b-hatena-entrylist.xml')
     stub_request(:get, Settings.hatena_rss_url).
        to_return(:status => 200, :body => hatebu_response, :headers => {})
   end
 
+  after { Timecop.return }
+
   describe "#get_today_gist" do
     it "今日はてブされたgistのURLとはてブ総数を返すこと" do
-      @hatena.get_today_gist.should == []
+      @hatena.get_today_gist.length.should == 10
     end
   end
 
