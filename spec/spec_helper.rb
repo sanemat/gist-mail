26df17f718618989c6439e3dda3055468b5116e9
sanemat <o.gata.ken@gmail.com>
2013-06-11 11:07:18 +0900

    Setup coveralls

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,6 +6,8 @@ require 'rspec/autorun'
 require 'capybara/rspec'
 require 'factory_girl'
 require 'webmock/rspec'
+require 'coveralls'
+Coveralls.wear!('rails')
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
