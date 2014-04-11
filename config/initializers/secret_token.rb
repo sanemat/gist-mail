a0dd09131f29ee58632ff8c608afdde834e1fc91
Suguru Odai <ourqwers@gmail.com>
2014-04-11 23:49:06 +0900

    Use environment variable when run with production

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,18 +4,8 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-require 'securerandom'
+secret_token = Rails.env.production? ? ENV["SECRET_TOKEN"]
+                                     : '90c6b42ceee8555048a5542a1e4032c7b14bdf487777f36474316474e88a01604478db2ee23795551c70e3116917ac5ff73a6238612c53f624248efb5afbd770'
 
-def secure_token
-  token_file = Rails.root.join('.secret')
-  if File.exist?(token_file)
-    File.read(token_file).chomp
-  else
-    token = SecureRandom.hex(64)
-    File.write(token_file, token)
-    token
-  end
-end
-
-GistMail::Application.config.secret_key_base = secure_token
-Devise.secret_key = GistMail::Application.config.secret_token
+GistMail::Application.config.secret_key_base = secret_token
+Devise.secret_key = secret_token
